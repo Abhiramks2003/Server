e77ff91265b6bf0f42b982311761a040dd603275
ABHIRAM KRISHNA S <abhiramkrishna085@gmail.com>
2023-04-06 12:06:36 +0530

    Update

--- mouse.py
+++
@@ -1,7 +0,0 @@
-import pyautogui
-while True:
-    # Get the current mouse position, state and scroll
-    x, y = pyautogui.position()
-    print((x,y))
-    # is_left_pressed, is_middle_pressed, is_right_pressed = pyautogui.mouseDown()
-    pyautogui.scroll(1)
